66e341810fdc66be40f8ed6c2b1d55134a40bd46
anonym-266 <desmondtardzenyuy@gmail.com>
2025-05-05 04:49:38 +0100

    added getters and setters for Student class

--- src/Student.java
+++ src/Student.java
@@ -3,4 +3,29 @@ public class Student {
   int age;
   int id;
   char grade;
+  
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public char getGrade() {
+    return grade;
+  }
+  public void setGrade(char grade) {
+    this.grade = grade;
+  }
 }
