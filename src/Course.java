3f5e6b760006789a538a22ba58316e32f784607c
anonym-266 <desmondtardzenyuy@gmail.com>
2025-05-06 13:42:59 +0100

    updated Course Class to autoomatically generate course code

--- src/Course.java
+++ src/Course.java
@@ -1,4 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Course {
+  private static final Map<String, Integer> departmentCounters = new HashMap<>();
+    private static final int DEFAULT_SUFFIX_START = 101;
   private String name;
   private String code;
   private String description;
@@ -13,10 +18,6 @@ public class Course {
 
   public String getCode() {
     return code;
-  }
-
-  public void setCode(String code) {
-    this.code = code;
   }
 
   public String getDescription() {
@@ -44,4 +45,25 @@ public String toString() {
     this.code = code;
     this.description = description;
   }
+
+  public static String generateCode(String subject) {
+    String department = extractDepartmentPrefix(subject);
+    
+    int suffix = departmentCounters.getOrDefault(department, DEFAULT_SUFFIX_START);
+    
+    String code = String.format("%s%03d", department, suffix);
+    
+    departmentCounters.put(department, suffix + 1);
+    
+    return code;
+}
+
+private static String extractDepartmentPrefix(String subject) {
+    String cleaned = subject.replaceAll("[^a-zA-Z]", "").toUpperCase();
+    
+    if (cleaned.length() < 4) {
+        return String.format("%-4s", cleaned).replace(' ', '0');
+    }
+    return cleaned.substring(0, 4);
+}
 }
