c015e5dc9599bed8a47f2542b140b67e299c8859
anonym-266 <desmondtardzenyuy@gmail.com>
2025-05-08 03:23:42 +0100

    added GraduateStudent class

--- src/student/GraduateStudent.java
+++ src/student/GraduateStudent.java
@@ -1,6 +1,7 @@
 package student;
 
 public class GraduateStudent extends Student {
+  private String thesisTitle, advisorName;
 
   public GraduateStudent(String name, int age) {
     super(name, age);
