974b4017ebcaf231a1a4707a96f8face5ba269d8
anonym-266 <desmondtardzenyuy@gmail.com>
2025-05-08 04:16:07 +0100

    update: added support for points in grade enum

--- src/student/Grade.java
+++ src/student/Grade.java
@@ -1,33 +1,31 @@
 package student;
 
 public enum Grade {
-  A("A"),
-  A_MINUS("A-"),
-  A_PLUS("A+"),
-  B("B"),
-  B_MINUS("B-"),
-  B_PLUS("B+"),
-  C("C"),
-  C_MINUS("C-"),
-  C_PLUS("C+"),
-  D("D"),
-  D_MINUS("D-"),
-  D_PLUS("D+"),
-  E("E"),
-  E_MINUS("E-"),
-  E_PLUS("E+"),
-  F("F"),
-  F_MINUS("F-"),
-  F_PLUS("F+"),;
+  A("A", 4.0),
+  A_MINUS("A-", 3.7),
+  B_PLUS("B+", 3.3),
+  B("B", 3.0),
+  B_MINUS("B-", 2.7),
+  C_PLUS("C+", 2.3),
+  C("C", 2.0),
+  C_MINUS("C-", 1.7),
+  D("D", 1.0),
+  F("F", 0.0);
 
   private final String display;
+  private final double points;
 
-  Grade(String display) {
-    this.display = display;
+  Grade(String display, double points) {
+      this.display = display;
+      this.points = points;
+  }
+
+  public double toPoints() {
+      return points;
   }
 
   @Override
   public String toString() {
-    return display;
+      return display;
   }
 }
