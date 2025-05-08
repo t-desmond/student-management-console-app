3098ded6596c029c956571cc178ba6378ca1c98e
anonym-266 <desmondtardzenyuy@gmail.com>
2025-05-08 03:23:02 +0100

    feature: added person class

---
+++ src/Person.java
@@ -0,0 +1,31 @@
+import java.util.UUID;
+
+public abstract class Person {
+  protected final UUID id;
+  protected String name;
+  protected int age;
+
+  public UUID getId() {
+    return id;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getAge() {
+    return age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public abstract String displayInfo();
+
+  Person(String name, int age) {
+    this.id = UUID.randomUUID();
+    this.name = name;
+    this.age = age;
+  }
+}
