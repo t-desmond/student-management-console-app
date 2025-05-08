6eb5addf4daba52f86fad462b06c0aa81987059a
anonym-266 <desmondtardzenyuy@gmail.com>
2025-05-08 03:37:33 +0100

    update: extended Student class

--- src/student/GraduateStudent.java
+++ src/student/GraduateStudent.java
@@ -3,8 +3,33 @@ package student;
 public class GraduateStudent extends Student {
   private String thesisTitle, advisorName;
 
-  public GraduateStudent(String name, int age) {
+  public GraduateStudent(String name, int age, String thesisTitle, String advisorName) {
     super(name, age);
+    this.thesisTitle = thesisTitle;
+    this.advisorName = advisorName;
   }
-  
+
+  public String getThesisTitle() {
+    return thesisTitle;
+  }
+
+  public void setThesisTitle(String thesisTitle) {
+    this.thesisTitle = thesisTitle;
+  }
+
+  public String getAdvisorName() {
+    return advisorName;
+  }
+
+  public void setAdvisorName(String advisorName) {
+    this.advisorName = advisorName;
+  }
+
+  @Override
+  public String displayInfo() {
+    return super.displayInfo() +
+        "Thesis Title: " + thesisTitle + "\n" +
+        "Advisor Name: " + advisorName + "\n";
+  }
+
 }
