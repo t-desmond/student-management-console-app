478abc1507a19d56484430912ed7dcb2bd77935b
anonym-266 <desmondtardzenyuy@gmail.com>
2025-05-08 03:41:38 +0100

    update added getter and setter for name field

--- src/School.java
+++ src/School.java
@@ -51,4 +51,12 @@ public class School {
   public School(String name) {
     this.name = name;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
